39ac3bb1149c240837bb82860c756fcf27b5f9c4
[Charlotte Chiraghuddin] <[charlottechiraghuddinxo@gmail.com]>
2026-03-06 01:11:34 +0000

    Added a requirements.txt file to ensure easy deployment

--- controllers/food_controller.py
+++ controllers/food_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, redirect, session, url_for
+from flask import Blueprint, render_template, request, redirect, session, url_for
 from models.food import Food
 from models.repository import Repository
 
@@ -25,3 +25,16 @@ def submit_food():
     repo.add_food(journal_day_id, name, calories, meal_type)
 
     return redirect(url_for('logbook.logbook_home')) #LATER ADD A FUNCTION TO REDIRECT TO TODAYS LOGS FOR FOOD.
+
+#@food_bp.route("/view_food")
+#def view_food():
+ #   user_id = session.get("user_id")
+  #  foods_daily = repo.get_user_data_daily(user_id)
+   # food_weekly = repo.get_user_data_weekly(user_id)
+    #food_monthly = repo.get_user_data_monthly(user_id)
+    #foods_yearly = repo.get_user_data_yearly(user_id)
+    #print("DAILY:", foods_daily)
+    #print("WEEKLY:", food_weekly)
+    #print("MONTHLY:", food_monthly)
+    #print("YEARLY:", foods_yearly)
+    #return render_template("view_food.html", foods_daily=foods_daily, food_weekly=food_weekly, food_monthly=food_monthly, foods_yearly=foods_yearly)
